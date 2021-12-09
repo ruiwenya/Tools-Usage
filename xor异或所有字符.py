4e0a7f4d46a593bd501af3331e43e5709684573b
ruiwenya <57943536+ruiwenya@users.noreply.github.com>
2021-12-09 15:04:45 +0800

    Delete xor异或所有字符.py

--- xor异或所有字符.py
+++
@@ -1,9 +0,0 @@
-import string
-from base64 import *
-
-flag = ''
-codestr = b64decode('AAoHAR1TIiIkUFUjUFQgVyInVSVQJVFRUSNRX1YgXiJSVyJQVRs=')
-for char in string.printable:
-    for num in codestr:
-        flag += chr(ord(char)^num)
-    print(flag)
